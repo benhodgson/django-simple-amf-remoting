12d7980cf102c05099b83a434e27a9ef05dce2a4
Ben Hodgson <ben@benhodgson.com>
2010-07-23 20:18:41 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='django-simple-amf-remoting',
+    version='0.1',
+    license='Public Domain',
+    description='A simplified API for AmFast\'s AMF remoting functionality, '
+        'designed to make setting up AMF remoting with Django quick and easy.'
+    package_dir = {'':'src'},
+    py_modules=['django_amf_remoting'],
+    url='http://github.com/benhodgson/django-simple-amf-remoting',
+    author='Ben Hodgson',
+    author_email='ben@benhodgson.com',
+    keywords = ['django', 'amf', 'amf0', 'amf3', 'flash', 'remoteobject',
+        'rpc', 'remoting', 'pyamf', 'amfast'],
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
